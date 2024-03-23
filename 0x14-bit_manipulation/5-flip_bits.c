37dea761ccb82900e4f7c54e33cadeeafc0f748a
simlexx-k <simonwriter254@gmail.com>
2024-03-23 07:37:17 -0400

    flip_bits function

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * flip_bits -Function that counts the number of bits to change
+ * to get from one number to another
+ * @n: 1st number
+ * @m: 2nd number
+ * Return: returns the number of bits to change
+ */
