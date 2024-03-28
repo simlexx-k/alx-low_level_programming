0f254e864cd13d266fc7c14bc0686cff13367370
simlexx-k <simonwriter254@gmail.com>
2024-03-28 12:40:04 -0700

    finalize the task

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A sample program to test the _strlen function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (0);
+}
